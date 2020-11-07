fca8d545546853265714d308c9866dabf9654472
mila <milarabello.dias@gmail.com>
2020-11-07 23:45:18 +0000

    deploy

--- src/http/index.js
+++ src/http/index.js
@@ -4,6 +4,6 @@ import axios from "axios";
  * Axios api
  */
 export default axios.create({
-  baseURL: " http://localhost:3000/",
+  baseURL: "https://my-json-server.typicode.com/MilaRCDias/Check-in-form/db",
   timeout: 1000,
 });
